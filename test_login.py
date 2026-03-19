f1c9ab71de3bdee3b9b1640c6e6b1d7b913b64bd
Lin Lin1 Jianjia <jianjialin1@Lenovo.com>
2026-03-19 18:36:23 +0800

    feat:add img read

--- test_login.py
+++
@@ -1,9 +0,0 @@
-from common.request_util import RequestUtil
-
-
-def test_login():
-    url = 'https://reqres.in/api/login'
-    data = {"email": "eve.holt@reqres.in", "password": "cityslicka"}
-    res = RequestUtil.send_request('post',url, json=data)
-    assert "token" in res.json()
-   
